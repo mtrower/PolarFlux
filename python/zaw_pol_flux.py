9c95d03e6e1e68e202f0fc18af907f7131b93a78
Zach Werginz <zachary.werginz@snc.edu>
2016-07-25 08:29:17 -0500

    Continued working on polar flux script

--- python/zaw_pol_flux.py
+++ python/zaw_pol_flux.py
@@ -62,19 +62,66 @@ def calc_pol(data, mgnt, pole):
 		pc_px_neg = np.where(mgnt.rg < mgnt.rsun and mgnt.lath < -deg_lim 
 						and mgnt.im_corr < 0.0)
 
+	# Search for polarity mixture.
 	if (np.size(pc_px_pos) == 0 or np.size(pc_px_neg) == 0):
 		print ("%s hemisphere has no polarity mixture.", %pole)
 		if pole == 'north':
 			n_swt = 3
 		else:
 			s_swt = 3
+
+	# Disregard magnetograms under the tolerance level for valid pixels.
 	if (float(np.size(vpc_px_ind))/float(np.size(pc_px_ind)) < inv_px_tol):
-		print ("%s hemisphere has more than %s %% invalid pixels.", %(pole, 1.0 - (inv_px_tol)*100))
+		print ("%s hemisphere has more than %s %% invalid pixels.", 
+				%(pole, 1.0 - (inv_px_tol)*100))
 		if pole == 'north':
 			n_swt = 2
 		else:
 			s_swt = 2
+
+	# Return with no data if the two previous cases are true.
 	if n_swt != 0 or s_swt != 0:
 		data['mdi_i'] = mgnt.mdi_i
 		data['date'] = mgnt.date
 		return data
+	# Otherwise calculate data from the poles.
+	else:
+		intf = np.nanmean(mgnt.im_raw[vpc_px_ind])
+    	intfc = np.nanmean(mgnt.im_corr[vpc_px_ind])
+    	unsflux = np.nansum(np.abs(mgnt.mflux_raw[vpc_px_indn]))
+        unsfluxc = np.nansum(np.abs(mgnt.mflux_corr[vpc_pxindn]))
+        sflux = np.nansum(mgnt.mflux_raw[vpc_px_ind])
+        sfluxc = np.nansum(mgnt.mflux_corr[vpc_px_ind])
+        posfluxc = np.nansum(mgnt.mflux_corr[pc_px_posn])
+        negfluxc = np.nansum(mgnt.mflux_corr[pc_px_neg])
+        visarea = np.nansum(mgnt.area[vpc_px_ind])
+        max_pxflux = np.nanmax(np.abs( mgnt.mflux_corr[vpc_px_ind]))
+        max_pxf = np.nanmax(mgnt.im_raw.data[vpc_px_ind])
+        max_pxfc =  np.nanmax(mgnt.im_corr[vpc_px_ind])
+        # TODO : use a for loop and a list with concatenation to simplifiy this code below.
+       	if pole == 'north':
+       		data['intf_n'] = intf
+       		data['intfc_n'] = intfc
+       		data['unsflux_n'] = unsflux
+       		data['unsfluxc_n'] = unsfluxc
+       		data['sflux_n'] = sflux
+       		data['sfluxc_n'] = sfluxc
+       		data['posfluxc_n'] = posfluxc
+       		data['negfluxc_n'] = negfluxc
+       		data['visarea_n'] = visarea
+       		data['max_pxflux_n'] = max_pxflux
+       		data['max_pxf'] = max_pxf
+       		data['max_pxfc'] = max_pxfc
+       	else:
+       		data['intf_n'] = intf
+       		data['intfc_n'] = intfc
+       		data['unsflux_n'] = unsflux
+       		data['unsfluxc_n'] = unsfluxc
+       		data['sflux_n'] = sflux
+       		data['sfluxc_n'] = sfluxc
+       		data['posfluxc_n'] = posfluxc
+       		data['negfluxc_n'] = negfluxc
+       		data['visarea_n'] = visarea
+       		data['max_pxflux_n'] = max_pxflux
+       		data['max_pxf'] = max_pxf
+       		data['max_pxfc'] = max_pxfc
